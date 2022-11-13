c484897c55369c48535e2054f7fac2ffba4ecab2
seuncarta <seuncarta@gmail.com>
2022-11-13 16:31:15 +0100

    created the book lists as and array and connected it to the component

--- src/index.js
+++ src/index.js
@@ -18,95 +18,103 @@ formatting
 
 */
 
+//Object 
+
+const books = [
+  {id: 1,
+    img: "https://m.media-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ducimus quibusdam labore quasi tenet"
+  }
+  ,
+  {id: 2,
+    img: "https://m.media-amazon.com/images/I/61aCVDVZPzL._SX484_BO1,204,203,200_.jpg",
+    title: "How to Catch a Turkey",
+    author: "Adam Wallace",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ducimus quibusdam labore quasi tenet"
+  },
+  {id: 3,
+    img: "https://m.media-amazon.com/images/I/51VC+Vru96L._SX320_BO1,204,203,200_.jpg",
+    title: "It Starts with Us",
+    author: "Colleen Hoover",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ducimus quibusdam labore quasi tenet"
+  }
+  ,
+  {id: 4,
+    img: "https://m.media-amazon.com/images/I/51ECRZXoGyL._SX327_BO1,204,203,200_.jpg",
+    title: "Fairy Tale",
+    author: "Stephen King",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ducimus quibusdam labore quasi tenet"
+  },
+  {id: 5,
+    img: "https://m.media-amazon.com/images/I/41mBCtaJ1XL._SX320_BO1,204,203,200_.jpg",
+    title: "Ugly Love",
+    author: "Colleen Hoover",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ducimus quibusdam labore quasi tenet"
+  },
+  {id: 6,
+    img: "https://m.media-amazon.com/images/I/41n9-p6-PpL._SX331_BO1,204,203,200_.jpg",
+    title: "Reminders of Him",
+    author: "Colleen Hoover",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ducimus quibusdam labore quasi tenet"
+  }
+]
 //Nested Components Feature
 
-const firstBook = {
-  img: "https://m.media-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
-  title: "I Love You to the Moon and Back",
-  author: "Amelia Hepworth"
-}
-
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/61aCVDVZPzL._SX484_BO1,204,203,200_.jpg",
-  title: "How to Catch a Turkey",
-  author: "Adam Wallace"
-}
-
-const thirdBook = {
-  img: "https://m.media-amazon.com/images/I/51VC+Vru96L._SX320_BO1,204,203,200_.jpg",
-  title: "It Starts with Us",
-  author: "Colleen Hoover"
-}
-
-const fourthBook = {
-  img: "https://m.media-amazon.com/images/I/51ECRZXoGyL._SX327_BO1,204,203,200_.jpg",
-  title: "Fairy Tale",
-  author: "Stephen King"
-}
-
-const fifthBook = {
-  img: "https://m.media-amazon.com/images/I/41mBCtaJ1XL._SX320_BO1,204,203,200_.jpg",
-  title: "Ugly Love",
-  author: "Colleen Hoover"
-}
-
-const sixthBook = {
-  img: "https://m.media-amazon.com/images/I/41n9-p6-PpL._SX331_BO1,204,203,200_.jpg",
-  title: "Reminders of Him",
-  author: "Colleen Hoover"
-}
+//Global Component
 
 function BookList() {
   return (
     <section className="bookList">
-      <Book
+      {books.map((book) => {
+        return (
+          <Book key={book.id} {...book}></Book>
+        );
+
+      })}
+
+      {/* <Book
         img={firstBook.img}
         title={firstBook.title}
         author={firstBook.author}
-      />
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-
-      <Book
-        img={thirdBook.img}
-        title={thirdBook.title}
-        author={thirdBook.author}
-      />
-
-      <Book
-        img={fourthBook.img}
-        title={fourthBook.title}
-        author={fourthBook.author}
-      />
-
-      <Book
-        img={fifthBook.img}
-        title={fifthBook.title}
-        author={fifthBook.author}
-      />
-
-      <Book
-        img={sixthBook.img}
-        title={sixthBook.title}
-        author={sixthBook.author}
-      />
+      >
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ducimus quibusdam labore quasi tenetur repellat
+        </p>
+      </Book> */}
     </section>
   );
 }
 
+// Props
 
-const Book = (madrid) => {
+// const Book = (props) => {
+//   const { img, title, author, description } = props.book
+//   return (
+//     <article className="book">
+//       <img style={{ width: "45%", height: "160px" }}
+//         src={img}
+//         alt="" />
+//       <h1>{title}</h1>
+//       <h4>{author}</h4>
+//       <p>{description}</p>
+//       {/* {children} */}
+//     </article>
+
+//   );
+// }
+
+const Book = ({ img, title, author, description }) => {
   return (
     <article className="book">
-      <img style={{ width: "45%", height:"160px" }}
-        src={madrid.img}
+      <img style={{ width: "45%", height: "160px" }}
+        src={img}
         alt="" />
-      <h1>{madrid.title}</h1>
-      <h4>{madrid.author}</h4>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <p>{description}</p>
+      {/* {children} */}
     </article>
 
   );
