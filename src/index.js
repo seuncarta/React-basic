d31a9249ffe42d07bf50171ccb81a3feac014336
seuncarta <seuncarta@gmail.com>
2022-11-13 08:59:36 +0100

    Nexting a component inside another component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 import React from "react";
 import ReactDom from 'react-dom';
 
+//statelss functional component
+//always return JSX
 
-function Greeting(){
-  return <h4>This is Carta and this is my first component</h4>;
+/*
+JSX Rules
+return single element
+div /section / article or fragment
+usecamelCase for huml attribute
+className instead of class
+close every element
+formatting
+
+*/
+
+//Nested Components Feature
+
+function Greeting() {
+  return (
+    <section>
+      <div>
+        <Person />
+        <Message />
+        {/* <ul>
+          <li>
+            <a href="#">Hello Guys</a>
+          </li>
+          <input type="text" />
+        </ul> */}
+      </div>;
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting/>, document.getElementById('root'));
+const Person = () => <h2>Seun Carta</h2>;
+const Message = () => {
+  return <p>This is my first component</p>
+};
+
+
+// const Greeting = () => {
+//   return React.createElement("div", {}, React.createElement("h1", {}, "Helllo World"));
+// }
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
 
