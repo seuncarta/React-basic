1f642e3b699d2ba5697eac296b611b61b741d7e3
seuncarta <seuncarta@gmail.com>
2022-11-17 18:58:32 +0100

    add an onclick event listener

--- src/index.js
+++ src/index.js
@@ -106,12 +106,18 @@ function BookList() {
 // }
 
 const Book = ({ img, title, author, description }) => {
+  //attribute, event handler 
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("Hello");
+  }
   return (
     <article className="book">
       <img style={{ width: "45%", height: "160px" }}
         src={img}
         alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={clickHandler}>{title}</h1>
       <h4>{author}</h4>
       <p>{description}</p>
       {/* {children} */}
